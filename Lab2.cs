aa7fc1d26c478a6318bbd2b9c741e5ea45a3ea5e
RudraDevan G <105837233+RudraDevaDev@users.noreply.github.com>
2025-02-05 20:53:11 +0530

    Update Lab2.cs

--- Lab2.cs
+++ Lab2.cs
@@ -1,26 +1,23 @@
-public class MyClass
-{
-public static void Main()
-{
-int num1 = 6;
-int num2 = 23;
-int res = num1 + num2;
-if (res > 25)
-{
-res = res - 5;
-Console.WriteLine("Result is more then 25");
-}
-else
-{
-res = 25;
-Console.WriteLine("Result is less then 25");
-}
-bool b = true;
-if (res > 25 || b)
-Console.WriteLine("Res > 25 or b is true");
-else if ( (res>25) && !b )
-Console.WriteLine("Res > 25 and b is false");
-else
-Console.WriteLine("else condition");
-}
+public class MyClass{
+  public static void Main(){
+    int num1 = 6;
+    int num2 = 23;
+    int res = num1 + num2;
+    if (res > 25){
+      res = res - 5;
+      Console.WriteLine("Result is more then 25");
+    }
+    else{
+      res = 25;
+      Console.WriteLine("Result is less then 25");  
+    }
+    bool b = true;
+    if (res > 25 || b)
+      Console.WriteLine("Res > 25 or b is true");
+    else if ( (res>25) && !b )
+      Console.WriteLine("Res > 25 and b is false");
+    else
+      Console.WriteLine("else condition");
+  
+  }
 }
